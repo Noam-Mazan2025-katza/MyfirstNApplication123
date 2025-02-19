c2ad156412821e20ae4b8946918f30567943eda6
NoamMazan <noam00707@gmail.com>
2025-02-19 19:44:15 +0200

    menu

--- app/src/main/java/com/noammazan/myfirstnapplication/MainActivity.java
+++ app/src/main/java/com/noammazan/myfirstnapplication/MainActivity.java
@@ -1,11 +1,16 @@
 package com.noammazan.myfirstnapplication;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -17,29 +22,25 @@ import android.graphics.Color;
 
 public class MainActivity extends AppCompatActivity {
     private static final int START_GAME = 2222;
-    Button btn1,btn2,btnLinear,btn3;
-Switch switchColor;
-ConstraintLayout layout;
+    Button btn1, btn2, btnLinear, btn3;
+    Switch switchColor;
+    ConstraintLayout layout;
     SeekBar sb;
     ImageView im;
     TextView tvAttempts;
+    TextView tv1;
 
-
-TextView tv1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         initView();
-
-        setContentView(R.layout.activity_main);
 
         // אתחול של ה-TextView בתוך המתודה onCreate
         tvAttempts = findViewById(R.id.textView5);
-
-        // שאר הקוד שלך
-        initView();
     }
 
     private void initView() {
@@ -47,7 +48,7 @@ TextView tv1;
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "היידד1" , Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, "היידד1", Toast.LENGTH_LONG).show();
             }
         });
 
@@ -55,18 +56,18 @@ TextView tv1;
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "היידד2" , Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, "היידד2", Toast.LENGTH_LONG).show();
             }
         });
 
-        tv1 =findViewById(R.id.textView2);
-        layout= findViewById(R.id.constraintLayout);
+        tv1 = findViewById(R.id.textView2);
+        layout = findViewById(R.id.constraintLayout);
 
         switchColor = findViewById(R.id.switchChangeColor);
         layout.setBackgroundColor(0xFFAB99CC);
         switchColor.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 if (switchColor.isChecked()) {
                     layout.setBackgroundColor(Color.BLACK); // צבע חדש
                 } else {
@@ -74,37 +75,33 @@ TextView tv1;
                 }
             }
         });
-        im=findViewById(R.id.im);
+
+        im = findViewById(R.id.im);
         sb = findViewById(R.id.seekBar);
         sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                float alpha =(float) i/100;
+                float alpha = (float) i / 100;
                 im.setAlpha(alpha);
             }
 
             @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
+            public void onStartTrackingTouch(SeekBar seekBar) {}
 
             @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
 
-            }
-        }
-        );
-
-        btnLinear= findViewById(R.id.button123);
+        btnLinear = findViewById(R.id.button123);
         btnLinear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent= new Intent(MainActivity.this, LinearActivity.class);
+                Intent intent = new Intent(MainActivity.this, LinearActivity.class);
                 startActivity(intent);
                 finish();
             }
         });
+
         btn3 = findViewById(R.id.button3);
         btn3.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -118,32 +115,35 @@ TextView tv1;
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-        // בדוק אם מדובר בבקשה שהתחלתה מתוך START_GAME
         if (requestCode == START_GAME && resultCode == RESULT_OK && data != null) {
-            // קבלת כמות הניסיונות שהוחזרה מ-GuessingNumberActivity
             int attempts = data.getIntExtra("attemptCount", 0); // 0 הוא ערך ברירת המחדל אם אין נתון
-
-            // הצגת כמות הניסיונות ב-TextView
             tvAttempts.setText("Number of attempts: " + attempts);
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
-
-    public void myfistClick(View view) {
-
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.item1:
+                Toast.makeText(this, "קטגוריה 1 נבחרה", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item2:
+                Toast.makeText(this, "קטגוריה 2 נבחרה", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item3:
+                Toast.makeText(this, "קטגוריה 3 נבחרה", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-
-/*    @Override
-    public void onClick(View view) {
-        if(view==btn1){
-            Toast.makeText(this, "היידד1" , Toast.LENGTH_LONG).show();
-            Toast.makeText(this, "היידד2" , Toast.LENGTH_LONG).show();
-        }
-
-        else if(view==btn2)
-            Toast.makeText(this, "היידד2" , Toast.LENGTH_LONG).show();
-    } */
+    }
 }
